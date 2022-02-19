32500ad4f258403ab1d27a1e3df1732a19c337ad
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-02-19 15:09:48 -0700

    save before going further with modularization

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,21 @@
-//write down routes for 
+const router = require('express').Router(); 
+const saveData = require('../db/saveData'); 
+const uuid = require('../helpers/uuid')
 // GET/api/notes -> read db.json
+router.get('/notes', function (req, res){
+    saveData
+    .retrieveNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
 // POST/api/notes -> read and append to json 
+router.post('/notes', (req, res) => {
+    saveData 
+    .addNote(req.body)
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
+
+// Bonus (delete)
+rous
 // README suggests using uuid to create unique id to save to json
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,4 @@
 // create a server.js file that will hold all info for back end. 
-
 //required files
 const express = require('express');
 const path = require('path');
@@ -10,19 +9,22 @@ const PORT = process.env.port || 3001;
 const app = express(); 
 
 // static paths???
+app.use(express.static('public'));
 
-//middleware 
 //parse JSON and URLencoded form data 
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json()); 
 
 // import routes 
 // GET route for index.html 
+app.use('/', htmlRoutes);
 // GET route for notes.html 
+app.use('/api', apiRoutes);
 // GET route for wildcard page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
 );
-
-
+//listener for port
 app.listen(PORT, ()=> 
 console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
