da9459d973a9eda31c28e53361b82b0590264082
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-02-22 08:25:27 -0700

    changed console info to console log

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,13 +5,13 @@ const uuid = require('../helpers/uuid');
 
 // GET route for retrieving notes: 
 router.get('/notes', (req, res) => {
-    console.info(`${req.method} request received to retrieve notes`);
+    console.log(`${req.method} request received to retrieve notes`);
     readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST route for writing new notes
 router.post('/notes', (req, res) => {
-    console.info(`${req.method} request received to add new notes`);
+    console.log(`${req.method} request received to add new notes`);
     console.log(req.body);
     const { title, text } = req.body;
     if (req.body) {
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 // get notes -> notes.html
 router.get('/notes', (req, res)=> {
-    console.info(`${req.method} request received for notes`);
+    console.log(`${req.method} request received for notes`);
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // GET homepage? -> index.html
 router.get('/', (req, res) => {
-    console.info(`${req.method} request received for homepage`);
+    console.log(`${req.method} request received for homepage`);
     res.sendFile(path.join(__dirname, '../public/index.html')); 
 });
 
